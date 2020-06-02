76bb1ab59bb0a4499db46546e1f09b694cfba1ca
Erick Navarro <erick@navarro.io>
2020-06-02 07:20:09 -0500

    Initial Commit

---
+++ gh-notifier/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  gh-notifier
+//
+//  Created by Erick Navarro on 6/2/20.
+//  Copyright © 2020 Erick Navarro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
